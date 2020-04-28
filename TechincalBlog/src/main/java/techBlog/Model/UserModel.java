b41bc3585c3564f1f056772d0cb6bc0fbab1e01b
Durgesh2398 <2308durgesh@gmail.com>
2020-04-28 14:30:04 +0530

    login

---
+++ TechincalBlog/src/main/java/techBlog/Model/UserModel.java
@@ -0,0 +1,14 @@
+package techBlog.Model;
+
+public class UserModel {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
